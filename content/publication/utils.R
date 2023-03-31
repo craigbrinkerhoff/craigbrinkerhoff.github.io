019662448d6b18884e3b3215c211c9a26cd46d77
craigbrinkerhoff <cbrinkerhoff@umass.edu>
2023-03-31 08:55:17 -0400

    added confluence paper

--- content/publication/utils.R
+++ content/publication/utils.R
@@ -14,17 +14,17 @@
 bibtexScraper <- function(i){
   library(bib2df)
   library(stringr)
-  
+
   # load in bibtex
-  papers <- bib2df('papers.bib')
-  urls <- as.character(papers$URL)
+  papers <- bib2df('my_papers.bib')
+  #urls <- as.character(papers$URL)
   
   paper <- papers[i,]#dplyr::filter(papers, URL==i)
     
   #handle DOIS leading with https
   doi <- paper$DOI
   doi <- ifelse(substr(doi, 1, 8)=='https://', substr(doi,9,length(doi)), doi)
-    
+
   #handle weird brackets in paper titles, abstracts, etc.
   title <- as.character(paper$TITLE)
   title <- str_remove_all(title, '[}{}?¶Ãâ€œˆ’]')
@@ -36,14 +36,20 @@ bibtexScraper <- function(i){
   authors <- str_remove_all(authors, '[}{}?¶Ãâ€œˆ’]')
   
   journal <- paper$JOURNAL
-    
+  journal <- str_remove_all(journal, '[}{}?¶Ãâ€œˆ’]')
+  
+  months = 1:12
+  names(months) = c('jan', 'feb', 'mar', 'apr', 'may','jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec')
+  month <- months[paper$MONTH]
+  month <- ifelse(nchar(month)==1, paste0('0',month), month)
+  date <- paste0(paper$YEAR, '-',month,'-01')
+
   #setup export object
   out <- list(
     'doi'=doi,
-    'url'=paper$URL,
     'title' = title,
     'journal'=journal,
-    'year'=paper$URLDATE, #dummy days, I just want the year
+    'year'=date,
     'authors' = authors,
     'abstract' = abstract
   )
